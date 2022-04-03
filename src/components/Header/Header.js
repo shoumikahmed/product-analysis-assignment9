c955fbfbcb3d6d835b4139c5a8a2c00a0c342622
shoumikahmed <shoumikahmed103@gmail.com>
2022-04-03 23:35:30 +0600

    home page added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     return (
         <div className='sticky top-0 bg-white px-12 py-8 flex justify-center md:justify-between'>
             <div className='hidden md:block'>
-                <Link to="/" className='font-sans font-bold text-xl text-grey-600'>Crypto Cafe</Link>
+                <Link to="/" className='font-bold text-xl text-grey-600'>Camera Review</Link>
             </div>
             <div className='flex gap-4 text-xl'>
                 <CustomLink to="/">Home</CustomLink>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <h1>This is home</h1>
+        <div className='flex justify-around items-center my-20'>
+            <div>
+                <h1 className='text-xl md:text-7xl'>Best camera</h1>
+                <h1 className='text-xl md:text-7xl'>Best capturing</h1>
+            </div>
+            <div className='w-[600px]'>
+                <img src="https://i.pinimg.com/originals/09/01/24/0901241fee529bc19918ad3b7b579826.jpg" alt="" />
+            </div>
         </div>
     );
 };
