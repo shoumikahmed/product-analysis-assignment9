f82d361fa36226a387e12912475d7f539fea7907
shoumikahmed <shoumikahmed103@gmail.com>
2022-04-03 23:53:14 +0600

    404 page added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,15 +2,18 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div className='flex justify-around items-center my-20'>
-            <div>
-                <h1 className='text-xl md:text-7xl'>Best camera</h1>
-                <h1 className='text-xl md:text-7xl'>Best capturing</h1>
+        <section>
+            <div className='flex justify-around items-center my-20'>
+                <div className='px-10'>
+                    <h1 className='text-xl md:text-7xl'>Best camera</h1>
+                    <h1 className='text-xl md:text-7xl text-purple-600'>Best capturing</h1>
+                    <p className='my-4'>That said, the D3500 will be too basic for experienced photographers. For a more powerful DSLR camera, we suggest looking at the Canon EOS 90D.</p>
+                </div>
+                <div className='w-[800px] px-10'>
+                    <img src="https://i.pinimg.com/originals/09/01/24/0901241fee529bc19918ad3b7b579826.jpg" alt="" />
+                </div>
             </div>
-            <div className='w-[600px]'>
-                <img src="https://i.pinimg.com/originals/09/01/24/0901241fee529bc19918ad3b7b579826.jpg" alt="" />
-            </div>
-        </div>
+        </section>
     );
 };
 
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className='flex justify-center items-center my-14'>
+            <h1 className='text-7xl text-purple-700'>404.Not Found!!!</h1>
+        </div>
+    );
+};
+
+export default NotFound;
