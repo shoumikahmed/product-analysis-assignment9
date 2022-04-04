18730e25b4aa3684339afe1a223460dad33c0b1f
shoumikahmed <shoumikahmed103@gmail.com>
2022-04-04 12:19:17 +0600

    2 charts completed

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,63 @@
 import React from 'react';
+import { Area, Bar, CartesianGrid, ComposedChart, Legend, Line, LineChart, Pie, PieChart, Scatter, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Dashboard = () => {
+    const data = [
+        {
+            "month": "Mar",
+            "investment": 100000,
+            "sell": 241,
+            "revenue": 10401
+        },
+        {
+            "month": "Apr",
+            "investment": 200000,
+            "sell": 423,
+            "revenue": 24500
+        },
+        {
+            "month": "May",
+            "investment": 500000,
+            "sell": 726,
+            "revenue": 67010
+        },
+        {
+            "month": "Jun",
+            "investment": 500000,
+            "sell": 529,
+            "revenue": 40405
+        },
+        {
+            "month": "Jul",
+            "investment": 600000,
+            "sell": 601,
+            "revenue": 50900
+        },
+        {
+            "month": "Aug",
+            "investment": 700000,
+            "sell": 670,
+            "revenue": 61000
+        }
+    ]
     return (
-        <div>
-            <h1>This is dashboard</h1>
+        <div className='my-10 ml-20 md:flex justify-around'>
+            <div>
+                <LineChart width={500} height={400} data={data}>
+                    <Line dataKey={'investment'}></Line>
+                    <Line dataKey={'revenue'}></Line>
+                    <XAxis dataKey={'month'}></XAxis>
+                    <Tooltip></Tooltip>
+                    <YAxis></YAxis>
+                </LineChart>
+            </div>
+            <div>
+                <PieChart width={500} height={500}>
+                    <Pie data={data} dataKey="revenue" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" />
+                    <Pie data={data} dataKey="investment" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label />
+                    <Tooltip></Tooltip>
+                </PieChart>
+            </div>
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
                 <div className='px-10'>
                     <h1 className='text-xl md:text-7xl'>Best camera</h1>
                     <h1 className='text-xl md:text-7xl text-purple-600'>Best capturing</h1>
-                    <p className='my-4'>That said, the D3500 will be too basic for experienced photographers. For a more powerful DSLR camera, we suggest looking at the Canon EOS 90D.</p>
+                    <p className='my-4'>That said, the D3500 will be too basic for experienced photographers. For a more powerful DSLR camera, we suggest looking at the Canon EOS 850D.</p>
                 </div>
                 <div className='w-[800px] px-10'>
                     <img src="https://i.pinimg.com/originals/09/01/24/0901241fee529bc19918ad3b7b579826.jpg" alt="" />
